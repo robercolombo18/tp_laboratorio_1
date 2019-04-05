918a7ef51388858f85da7b0b95984ebedb2ebfdd
robercolombo18 <colombo_rf1999@hotmail.com>
2019-04-05 15:59:53 -0300

    TP 1 Entrega

--- tp_laboratorio1/main.c
+++ tp_laboratorio1/main.c
@@ -4,30 +4,28 @@
 
 int main()
 {
-    char seguir='s';
-    int opcion=0;
-    float numero1=0;
-    float numero2=0;
+    char seguir= 's';
+    int opcion = 0;
+    int bandera = 0;
+    float numero1 = 0;
+    float numero2 = 0;
     float suma;
     float resta;
     float division;
     float multiplicacion;
-    float factorialResultado;
+    float factorialResultado1;
+    float factorialResultado2;
 
-    while(seguir=='s')
+    do
     {
-        printf("----------CALCULADORA----------\n\n");
-        printf("1- Ingresar 1er operando (A=%.1f)\n", numero1);
-        printf("2- Ingresar 2do operando (B=%.1f)\n", numero2);
-        printf("3- Calcular la suma (A+B)\n");
-        printf("4- Calcular la resta (A-B)\n");
-        printf("5- Calcular la division (A/B)\n");
-        printf("6- Calcular la multiplicacion (A*B)\n");
-        printf("7- Calcular el factorial (A!)\n");
-        printf("8- Calcular todas las operaciones\n");
-        printf("9- Salir\n");
-
-        scanf("%d",&opcion);
+        printf("\n----------CALCULADORA---------- \n\n");
+        printf("Seleccione una de las siguientes opciones: \n");
+        printf("1- Ingresar 1er operando: (A = %.1f)\n", numero1);
+        printf("2- Ingresar 2do operando: (B = %.1f)\n", numero2);
+        printf("3- Calcular todas las operaciones. \n");
+        printf("4- Informar resultados. \n");
+        printf("5- Salir. \n");
+        scanf("%d", &opcion);
 
         switch(opcion)
         {
@@ -35,115 +33,99 @@ int main()
                 system("cls");
                 printf("Ingresar 1er operando: \n");
                 scanf("%f", &numero1);
-                break;
+            break;
+
+
             case 2:
                 system("cls");
                 printf("Ingresar 2do operando: \n");
                 scanf("%f", &numero2);
-                break;
+            break;
+
+
             case 3:
                 system("cls");
                 suma = sumar(numero1, numero2);
-                printf("El resultado de %.2f + %.2f es: %.2f\n\n", numero1, numero2, suma);
-                break;
-            case 4:
-                system("cls");
                 resta = restar(numero1, numero2);
-                printf("El resultado de %.2f - %.2f es: %.2f\n\n", numero1, numero2, resta);
-                break;
+                division = dividir(numero1, numero2);
+                multiplicacion = multiplicar(numero1, numero2);
+                factorialResultado1 = factorial(numero1);
+                factorialResultado2 = factorial(numero2);
+                printf("Se han realizado los calculos exitosamente. \n\n");
+                bandera = 1;
+
+            break;
+
+
+            case 4:
+
+                if(bandera == 1)
+                {
+                    system("cls");
+
+                    //SUMA
+                    printf("El resultado de %.2f + %.2f es: %.2f \n", numero1, numero2, suma);
+
+                    //RESTA
+                    printf("El resultado de %.2f - %.2f es: %.2f \n", numero1, numero2, resta);
+
+                    //DIVISION
+                    if(numero2 == 0)
+                    {
+                        printf("No se puede dividir por cero. \n");
+                    }
+                    else
+                    {
+                        printf("El resultado de %.2f / %.2f es: %.2f \n", numero1, numero2, division);
+                    }
+
+                    //MULTIPLICACION
+                    printf("El resultado de %.2f * %.2f es: %.2f \n", numero1, numero2, multiplicacion);
+
+                    //FACTORIAL
+                    if(numero1 >= 0 && numero2 >= 0)
+                    {
+                        printf("El factorial de %.2f es: %.2f y el factorial de %.2f es: %.2f \n", numero1, factorialResultado1, numero2, factorialResultado2);
+                    }
+                    else if(numero1 >= 0 && numero2 < 0)
+                    {
+                        printf("El factorial de %.2f es: %.2f y el factorial de %.2f no existe. \n", numero1, factorialResultado1, numero2);
+                    }
+                    else if(numero1 < 0 && numero2 >= 0)
+                    {
+                        printf("El factorial de %.2f no existe y el factorial de %.2f es: %.2f \n", numero1, numero2, factorialResultado2);
+                    }
+                    else if(numero1 < 0 && numero2 < 0)
+                    {
+                        printf("El factorial de %.2f no existe y el factorial de %.2f tampoco. \n\n", numero1, numero2);
+                    }
+
+                }
+                else
+                {
+                    system("cls");
+                    printf("No se pueden mostrar los resultados sin antes haberlos calculados. \n\n");
+                }
+
+            break;
+
+
             case 5:
                 system("cls");
-                if(numero2 == 0)
-                {
-                    printf("No se puede dividir por cero.\n\n");
-                }
-                else
-                {
-                division = dividir(numero1, numero2);
-                printf("El resultado de %.2f / %.2f es: %.2f\n\n", numero1, numero2, division);
-                }
-                break;
-            case 6:
-                system("cls");
-                multiplicacion = multiplicar(numero1, numero2);
-                printf("El resultado de %.2f * %.2f es: %.2f\n\n", numero1, numero2, multiplicacion);
-                break;
-            case 7:
-                system("cls");
-                if(numero1 < 0)
-                {
-                    printf("No existe el factorial de %.2f\n\n", numero1);
-                }
-                else
-                {
-                factorialResultado = factorial(numero1);
-                printf("El factorial de %.2f es %.2f\n\n", numero1, factorialResultado);
-                }
-
-                if(numero2 < 0)
-                {
-                    printf("No existe el factorial de %.2f\n\n", numero2);
-                }
-                else
-                {
-                factorialResultado = factorial(numero2);
-                printf("El factorial de %.2f es %.2f\n\n", numero2, factorialResultado);
-                }
-                break;
-            case 8:
-                system("cls");
-
-                suma = sumar(numero1, numero2);
-                printf("El resultado de %.2f + %.2f es: %.2f\n\n", numero1, numero2, suma);
-
-                resta = restar(numero1, numero2);
-                printf("El resultado de %.2f - %.2f es: %.2f\n\n", numero1, numero2, resta);
-
-                if(numero2 == 0)
-                {
-                    printf("No se puede dividir por cero.\n\n");
-                }
-                else
-                {
-                division = dividir(numero1, numero2);
-                printf("El resultado de %.2f / %.2f es: %.2f\n\n", numero1, numero2, division);
-                }
-
-                multiplicacion = multiplicar(numero1, numero2);
-                printf("El resultado de %.2f * %.2f es: %.2f\n\n", numero1, numero2, multiplicacion);
-
-                if(numero1 < 0)
-                {
-                    printf("No existe el factorial de %.2f\n\n", numero1);
-                }
-                else
-                {
-                factorialResultado = factorial(numero1);
-                printf("El factorial de %.2f es %.2f\n\n", numero1, factorialResultado);
-                }
-
-                if(numero2 < 0)
-                {
-                    printf("No existe el factorial de %.2f\n\n", numero2);
-                }
-                else
-                {
-                factorialResultado = factorial(numero2);
-                printf("El factorial de %.2f es %.2f\n\n", numero2, factorialResultado);
-                }
-                break;
-            case 9:
-                system("cls");
                 seguir = 'n';
-                printf("\nHas salido exitosamente.\n");
-                break;
+                printf("Ha salido exitosamente. \n\n");
+            break;
+
             default:
                 system("cls");
-                printf("La opcion es invalida.\n\n");
-                break;
+                printf("La opcion no es valida. \n\n");
+            break;
+
         }
 
+
     }
+    while(seguir != 'n');
 
 
     return 0;
